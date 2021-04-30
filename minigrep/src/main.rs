d97e0d3a672aa500befeb526272e0d57d51b1b12
Dhananjay Latkar <dhananjay.latkar@gmail.com>
2021-04-30 23:28:40 +0530

    minigrep - reading file and making code more modular

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,19 @@
-use std::env;
+use std::{env, process};
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
